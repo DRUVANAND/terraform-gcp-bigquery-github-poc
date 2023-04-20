653ee7096de5cb904d44a102dec5a3a54e627ef3
DHRUVOPS360 <128677270+DHRUVOPS360@users.noreply.github.com>
2023-04-20 20:12:51 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = "dhruv-dataset-github"
+  friendly_name               = "test"
+  description                 = "This is a dataset from Terraform script"
+  location                    = "US
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+}
+
+resource "google_bigquery_table" "default" {
+  dataset_id = google_bigquery_dataset.dataset.dataset_id
+  table_id   = "dhruv-table-github"
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  labels = {
+    env = "default"
+  }
+  deletion_protection=false
+}
